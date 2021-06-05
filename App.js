e97763adbb403f1dc7882653d837764cf2291bc7
michaelhk101 <56811528+michaelhk101@users.noreply.github.com>
2021-06-06 00:02:11 +0800

    Update App.js with useFonts from expo-font

--- App.js
+++ App.js
@@ -4,71 +4,68 @@ import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
-import * as Font from 'expo-font';
-import { Asset } from 'expo-asset';
-import { AppLoading } from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
-const fetchFonts = async () => {
-  return await Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-  });
-};
 
 export default () => {
-  const [userNumber, setUserNumber] = useState();
-  const [guessRounds, setGuessRounds] = useState(0);
-  const [dataLoaded, setDataLoaded] = useState(false);
-
-  if (!dataLoaded) {
-    try {
-      fetchFonts();
-      setDataLoaded(true);
-    } catch (err) {
-      console.log(err);
+    const [userNumber, setUserNumber] = useState();
+    const [guessRounds, setGuessRounds] = useState(0);
+    const [dataLoaded] = useFonts({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    });
+    if (!dataLoaded) {
+        return null;
     }
-  }
+    /*    {
+        try {
+          fetchFonts();
+          setDataLoaded(true);
+        } catch (err) {
+          console.log(err);
+        }
+      }*/
 
-  const configureNewGameHandler = () => {
-    setGuessRounds(0);
-    setUserNumber(null);
-  };
+    const configureNewGameHandler = () => {
+        setGuessRounds(0);
+        setUserNumber(null);
+    };
 
-  const startGameHandler = (selectedNumber) => {
-    setUserNumber(selectedNumber);
-    setGuessRounds(0);
-  };
+    const startGameHandler = (selectedNumber) => {
+        setUserNumber(selectedNumber);
+        setGuessRounds(0);
+    };
 
-  const gameOverHandler = (numOfRounds) => {
-    setGuessRounds(numOfRounds);
-  };
+    const gameOverHandler = (numOfRounds) => {
+        setGuessRounds(numOfRounds);
+    };
 
-  let content = <StartGameScreen onStartGame={startGameHandler} />;
+    let content = <StartGameScreen onStartGame={startGameHandler} />;
 
-  if (userNumber && guessRounds <= 0) {
-    content = (
-      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    if (userNumber && guessRounds <= 0) {
+        content = (
+            <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+        );
+    } else if (guessRounds > 0) {
+        content = (
+            <GameOverScreen
+                roundsNumber={guessRounds}
+                userNumber={userNumber}
+                onRestart={configureNewGameHandler}
+            />
+        );
+    }
+
+    return (
+        <SafeAreaView style={styles.screen}>
+            <Header title="Guess a Number" />
+            {content}
+        </SafeAreaView>
     );
-  } else if (guessRounds > 0) {
-    content = (
-      <GameOverScreen
-        roundsNumber={guessRounds}
-        userNumber={userNumber}
-        onRestart={configureNewGameHandler}
-      />
-    );
-  }
-
-  return (
-    <SafeAreaView style={styles.screen}>
-      <Header title="Guess a Number" />
-      {content}
-    </SafeAreaView>
-  );
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-  },
+    screen: {
+        flex: 1,
+    },
 });
